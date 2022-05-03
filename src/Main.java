9de7b54ebec56eb1ec5035717e548fc592214ef6
Stukotolog <stukotolog@mail.ru>
2022-05-03 19:54:57 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        int amount = 5555;
+
+        int mile = 20;
+
+        int bonus = amount / mile;
+
+
+        System.out.println("ваши бонусные мили: " + bonus);}
+}
